11e807a658e9ad797f7c863f8d3971f8b6101ceb
Preetam Kumar Chaurasiya <122163552+PreetamKumarChaurasiya@users.noreply.github.com>
2023-02-03 20:18:59 +0530

    Add files via upload

---
+++ cricket_score.py
@@ -0,0 +1,14 @@
+from pycricbuzz import Cricbuzz
+
+c = Cricbuzz()
+matches = c.matches()
+
+for match in matches:
+    print(match)
+    if(match['mchstate'] != 'nextlive'):
+        print(c.livescore(match['id']))
+        print(c.commentary(match['id']))
+        print(c.scorecard(match['id']))
+
+
+
